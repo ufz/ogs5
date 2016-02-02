5b8990fa23d0c5c4832c3fd0edabbfac6a8dd543
Lars Bilke <lars.bilke@ufz.de>
2016-02-02 11:27:41 +0100

    Cleanup of old scripts.

--- scripts/style_code.sh
+++
@@ -1,7 +0,0 @@
-cd styleguide
-for dir in "Base" "FEM" "FileIO" "GEO" "MathLib" "MSH" "MSHGEOTOOLS" "OGS" "OGSProject" "Qt" "UTL"
-do
-	./uncrustify.sh ../../"$dir" h
-	./uncrustify.sh ../../"$dir" cpp
-done
-cd ..
