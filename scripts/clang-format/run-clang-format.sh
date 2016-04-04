a5a4677838508cdefc857c0a78a87876c700865d
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-04-04 07:09:14 +0200

    add -sort-includes=false to clang-format

--- scripts/clang-format/run-clang-format.sh
+++ scripts/clang-format/run-clang-format.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # run this script at the source root directory
-find . \( -name "*.h" -or -name "*.cpp" \) -print0 | xargs -0 clang-format -i -style=file
+
+# for CLANG <= 3.7
+#find . \( -name "*.h" -or -name "*.cpp" \) -print0 | xargs -0 clang-format -i -style=file
+
+# for CLANG > 3.7
+find . \( -name "*.h" -or -name "*.cpp" \) -print0 | xargs -0 clang-format -i -sort-includes=false -style=file
+
