05f52afd91d01caf1e5b8efca522832cec666a10
Lars Bilke <lars.bilke@ufz.de>
2016-06-06 11:37:46 +0200

    Use FindPackageHandleStandardArgs in FindLIS.cmake

--- scripts/cmake/FindLIS.cmake
+++ scripts/cmake/FindLIS.cmake
@@ -5,39 +5,29 @@
 #  LIS_INCLUDE_DIRS
 #  LIS_LIBRARIES
 
-if (NOT LIS_FOUND)
+find_path( LIS_INCLUDE_DIR
+	NAMES lis.h
+	PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled)
 
-	include(LibFindMacros)
-
-	find_path( LIS_INCLUDE_DIR
-		NAMES lis.h
-		PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled)
-
-	if ( UNIX )
-		# Tell if the unix system is on 64-bit base
-		if(CMAKE_SIZEOF_VOID_P MATCHES "8")
-			find_library(LIS_LIBRARIES
-				NAMES lis-64
-				PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled )
-		else ()
-			find_library(LIS_LIBRARIES
-				NAMES lis-32
-				PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled )
-		endif ()
+if ( UNIX )
+	# Tell if the unix system is on 64-bit base
+	if(CMAKE_SIZEOF_VOID_P MATCHES "8")
+		find_library(LIS_LIBRARIES
+			NAMES lis-64
+			PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled )
 	else ()
 		find_library(LIS_LIBRARIES
-			NAMES lisomp
+			NAMES lis-32
 			PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled )
 	endif ()
-
-	# Set the include dir variables and the libraries and let libfind_process do the rest.
-	# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-	if (NOT LIS_LIBRARIES STREQUAL "LIS_LIBRARIES-NOTFOUND" AND NOT LIS_INCLUDE_DIR STREQUAL "LIS_INCLUDE_DIR-NOTFOUND")
-		set(LIS_PROCESS_INCLUDES LIS_INCLUDE_DIR)
-		set(LIS_PROCESS_LIBS LIS_LIBRARIES)
-		libfind_process(LIS)
-	else ()
-		message (STATUS "Warning: LIS not found!")
-	endif ()
-
+else ()
+	find_library(LIS_LIBRARIES
+		NAMES lisomp
+		PATHS ${CMAKE_SOURCE_DIR}/../Libs/precompiled )
 endif ()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LIS
+	REQUIRED_VARS LIS_INCLUDE_DIR LIS_LIBRARIES
+	FOUND_VAR LIS_FOUND
+)
