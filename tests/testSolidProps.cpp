382b7605272b0ee766c841788be410cdb5e42a77
tnagel <thomas.nagel@ufz.de>
2016-06-21 18:48:39 +0200

    Introduced Minkley residual test. Works.

--- tests/testSolidProps.cpp
+++ tests/testSolidProps.cpp
@@ -547,103 +547,113 @@ TEST(SolidProps, TensorInversion)
         ASSERT_NEAR(inv_t(i),inv(i),1.e-10);
 }
 
-//TEST(SolidProps, MinkleyFullResidual)
-//{
-//    SolidProp::CSolidProperties solid;
-//    SolidMath::Invariants smath;
-//    Math_Group::Matrix* data;
-//    data = new Math_Group::Matrix(15);
+TEST(SolidProps, MinkleyFullResidual)
+{
+    SolidProp::CSolidProperties solid;
+    SolidMath::Invariants smath;
+    Math_Group::Matrix* data;
+    data = new Math_Group::Matrix(15);
 
-//    //set Constants
-//    (*data)(0) = 63.e3; //Kelvin shear modulus
-//    (*data)(1) = 14.e6; //Kelvin viscosity
-//    (*data)(2) = 12.e3; //Maxwell shear modulus
-//    (*data)(3) = 18.e3; //Maxwell bulk modulus
-//    (*data)(4) = 10.e10; //Maxwell viscosity
-//    (*data)(5) = 4.9; //dependency parameter for " (m)
-//    (*data)(6) = 0.33; //dependency parameter for " (n)
-//    (*data)(7) = 2.; //cohesion
-//    (*data)(8) = 10.; //hardening
-//    (*data)(9) = 40.; //friction angle
-//    (*data)(10) = 10.; //dilatancy angle
-//    (*data)(11) = 28.; //transition angle
-//    (*data)(12) = 0.01; //regularisation
-//    (*data)(13) = 0.; // temperature parameter for Maxwell viscosity
-//    (*data)(14) = 0.; // reference temperature for Maxwell viscosity
-//    Minkley::SolidMinkley material(data);
+    //set Constants
+    (*data)(0) = 63.e3; //Kelvin shear modulus
+    (*data)(1) = 8.37e4; //Kelvin viscosity
+    (*data)(2) = 12.e3; //Maxwell shear modulus
+    (*data)(3) = 18.e3; //Maxwell bulk modulus
+    (*data)(4) = 4.03e7; //Maxwell viscosity
+    (*data)(5) = 4.9; //dependency parameter for " (m)
+    (*data)(6) = 0.33; //dependency parameter for " (n)
+    (*data)(7) = 2.; //cohesion
+    (*data)(8) = 100.; //hardening
+    (*data)(9) = 30.; //friction angle
+    (*data)(10) = 10.; //dilatancy angle
+    (*data)(11) = 29.; //transition angle
+    (*data)(12) = 0.01; //regularisation
+    (*data)(13) = 0.; // temperature parameter for Maxwell viscosity
+    (*data)(14) = 0.; // reference temperature for Maxwell viscosity
+    Minkley::SolidMinkley material(data);
 
-//    //state and trial variables
-//    Eigen::Matrix<double,6,1> eps_i, eps_K_t, eps_K_j, eps_pl_t, eps_pl_j;
-//    Eigen::Matrix<double,6,1> sig_t, sigd_j, sig_j, epsd_i, epsd_t;
-//    eps_i.setZero(6);
-//    eps_K_t.setZero(6); eps_K_j.setZero(6);
-//    eps_pl_t.setZero(6); eps_pl_j.setZero(6);
-//    sig_t.setZero(6); sigd_j.setZero(6); sig_j.setZero(6);
-//    epsd_t.setZero(6); epsd_i.setZero(6);
+    //state and trial variables
+    Eigen::Matrix<double,6,1> eps_i, eps_K_t, eps_K_j, eps_M_t, eps_M_j, eps_pl_t, eps_pl_j;
+    Eigen::Matrix<double,6,1> sig_t, sigd_j, sig_j, epsd_i, epsd_t;
+    eps_i.setZero(6);
+    eps_K_t.setZero(6); eps_K_j.setZero(6);
+    eps_M_t.setZero(6); eps_M_j.setZero(6);
+    eps_pl_t.setZero(6); eps_pl_j.setZero(6);
+    sig_t.setZero(6); sigd_j.setZero(6); sig_j.setZero(6);
+    epsd_t.setZero(6); epsd_i.setZero(6);
 
-//    //Residual vector
-//    Eigen::Matrix<double,21,1> residual;
+    //Residual vector
+    Eigen::Matrix<double,27,1> residual;
 
-//    //set a strain increment and perform Kelvin mapping
-//    double *strain_bc;
-//    strain_bc = new double[6];
-//    strain_bc[0] = 0.01; strain_bc[1] = 0.02; strain_bc[2] = 0.03;
-//    strain_bc[3] = 0.04*2.; strain_bc[4] = 0.05*2.; strain_bc[5] = 0.06*2.;
-//    eps_i = solid.Voigt_to_Kelvin_Strain(strain_bc);
-//    eps_i *= -0.01;
+    //set a strain increment and perform Kelvin mapping
+    double *strain_bc;
+    strain_bc = new double[6];
+    strain_bc[0] = 0.01; strain_bc[1] = 0.02; strain_bc[2] = 0.03;
+    strain_bc[3] = 0.04*2.; strain_bc[4] = 0.05*2.; strain_bc[5] = 0.06*2.;
+    eps_i = solid.Voigt_to_Kelvin_Strain(strain_bc);
+    eps_i *= -0.01;
 
-//    delete [] strain_bc;
+    delete [] strain_bc;
 
-//    epsd_i = smath.P_dev*eps_i;
+    epsd_i = smath.P_dev*eps_i;
 
-//    const double e_i(smath.CalI1(eps_i)), e_t(0.);
+    const double e_i(smath.CalI1(eps_i));
 
-//    //guess stress increment (dimensionless)
-//    sigd_j = 2.0 * epsd_i;
-//    sig_j = sigd_j + material.KM0/material.GM0 * e_i * smath.ivec;
+    //guess stress increment (dimensionless)
+    sigd_j = 2.0 * epsd_i;
+    sig_j = sigd_j + material.KM0/material.GM0 * e_i * smath.ivec;
 
-//    //Calculate residual for time step
-//    const double dt(10.);
-//    //do get nonzero flow driven residuals
-//    const double lam_j(0.01);
-//    eps_K_j = 0.1 * epsd_i;
-//    eps_pl_j = 0.2 * epsd_i;
-//    const double e_v_i = smath.CalI1(eps_pl_j);
-//    const double e_eff_i = 1.2*e_v_i;
+    //Calculate residual for time step
+    const double dt(10.);
+    //Modify plastic arrays to ensure full check
+    //do get nonzero flow driven residuals
+    const double lam_j(0.01);
+    eps_K_j = 0.1 * epsd_i;
+    eps_M_j = 0.15 * epsd_i;
+    eps_pl_j = 0.2 * epsd_i;
+    const double e_v_i = 0.2*e_i;
+    const double e_eff_i = 1.2*e_i;
 
-//    //Update Material parameters
-//    material.UpdateMinkleyProperties(smath.CalEffectiveStress(sigd_j * material.GM0),e_eff_i,0.);
+    //Update Material parameters
+    material.UpdateMinkleyProperties(smath.CalEffectiveStress(sigd_j * material.GM0),e_eff_i,0.);
 
-//    material.CalViscoplasticResidual(dt,epsd_i,epsd_t,e_i,e_t,sig_j,sig_t,eps_K_j,eps_K_t,eps_pl_j,eps_pl_t,e_v_i,0.,e_eff_i,0.,lam_j,residual);
+    material.CalViscoplasticResidual(dt,epsd_i,e_i,sig_j,eps_K_j,eps_K_t,eps_M_j,eps_M_t,eps_pl_j,eps_pl_t,e_v_i,0.,e_eff_i,0.,lam_j,residual);
 
-//    Eigen::Matrix<double,21,1> residual_Python;
+    Eigen::Matrix<double,27,1> residual_Python;
 
-//    //These values are taken from a constituive model test script coded in Python
-//    residual_Python( 0 ) =  0.000122806767162 ;
-//    residual_Python( 1 ) =  -1.08420217249e-19 ;
-//    residual_Python( 2 ) =  -0.000122806767162 ;
-//    residual_Python( 3 ) =  -0.000694699982687 ;
-//    residual_Python( 4 ) =  -0.000868374978359 ;
-//    residual_Python( 5 ) =  -0.00104204997403 ;
-//    residual_Python( 6 ) =  9.59285714286e-07 ;
-//    residual_Python( 7 ) =  -3.55173015254e-22 ;
-//    residual_Python( 8 ) =  -9.59285714286e-07 ;
-//    residual_Python( 9 ) =  -5.42653946933e-06 ;
-//    residual_Python( 10 ) =  -6.78317433667e-06 ;
-//    residual_Python( 11 ) =  -8.139809204e-06 ;
-//    residual_Python( 12 ) =  -0.00140743973352 ;
-//    residual_Python( 13 ) =  0.00173295588137 ;
-//    residual_Python( 14 ) =  -0.000325516147857 ;
-//    residual_Python( 15 ) =  0.00127776404536 ;
-//    residual_Python( 16 ) =  0.00237699541911 ;
-//    residual_Python( 17 ) =  0.00614979374969 ;
-//    residual_Python( 18 ) =  -0.00173648177667 ;
-//    residual_Python( 19 ) =  -0.00580244554726 ;
-//    residual_Python( 20 ) =  0.000556829512829 ;
+    //These values are taken from a constituive model test script coded in Python
+    residual_Python( 0 ) =  -9e-05 ;
+    residual_Python( 1 ) =  -0.00018 ;
+    residual_Python( 2 ) =  -0.00027 ;
+    residual_Python( 3 ) =  -0.000509116882454 ;
+    residual_Python( 4 ) =  -0.000636396103068 ;
+    residual_Python( 5 ) =  -0.000763675323681 ;
+    residual_Python( 6 ) =  -5.81003584229e-06 ;
+    residual_Python( 7 ) =  8.19126399401e-21 ;
+    residual_Python( 8 ) =  5.81003584229e-06 ;
+    residual_Python( 9 ) =  3.28665259442e-05 ;
+    residual_Python( 10 ) =  4.10831574302e-05 ;
+    residual_Python( 11 ) =  4.92997889163e-05 ;
+    residual_Python( 12 ) =  -0.144920047348 ;
+    residual_Python( 13 ) =  1.17842582492e-16 ;
+    residual_Python( 14 ) =  0.144920047348 ;
+    residual_Python( 15 ) =  0.819791585675 ;
+    residual_Python( 16 ) =  1.02473948209 ;
+    residual_Python( 17 ) =  1.22968737851 ;
+    residual_Python( 18 ) =  -0.00140743973352 ;
+    residual_Python( 19 ) =  0.00173295588137 ;
+    residual_Python( 20 ) =  -0.000325516147857 ;
+    residual_Python( 21 ) =  0.00127776404536 ;
+    residual_Python( 22 ) =  0.00237699541911 ;
+    residual_Python( 23 ) =  0.00614979374969 ;
+    residual_Python( 24 ) =  -0.00174848177667 ;
+    residual_Python( 25 ) =  -0.00587444554726 ;
+    residual_Python( 26 ) =  0.000736664715603 ;
 
-//    for (size_t i=0; i<21; i++)
-//        ASSERT_NEAR(residual(i),residual_Python(i),1.e-10);
-//}
+    for (size_t i=0; i<27; i++){
+        ASSERT_NEAR(residual(i),residual_Python(i),1.e-10);
+    }
+}
 
 TEST(SolidProps, MinkleyFullJacobian)
 {
@@ -709,8 +719,8 @@ TEST(SolidProps, MinkleyFullJacobian)
     eps_K_j = 0.1 * epsd_i;
     eps_M_j = 0.15 * epsd_i;
     eps_pl_j = 0.2 * epsd_i;
-    const double e_v_i = smath.CalI1(eps_pl_j);
-    const double e_eff_i = 1.2*e_v_i;
+    const double e_v_i = 0.2*e_i;
+    const double e_eff_i = 1.2*e_i;
 
     //Update Material parameters
     material.UpdateMinkleyProperties(smath.CalEffectiveStress(sigd_j * material.GM0),e_eff_i,0.);
@@ -727,8 +737,8 @@ TEST(SolidProps, MinkleyFullJacobian)
     const double pertub(1.e-8);
     double up, low;
 
-    for (size_t i=0; i<21; i++)
-        for (size_t j=0; j<21; j++)
+    for (size_t i=0; i<27; i++)
+        for (size_t j=0; j<27; j++)
         {
             if (j < 6)
             {
@@ -814,6 +824,7 @@ TEST(SolidProps, MinkleyFullJacobian)
                 low = residual(i);
             }
             Jac_num(i,j) = (up-low)/(2.*pertub);
+            std::cout << i << " " << j << std::endl;
             ASSERT_NEAR(Jacobian(i,j),Jac_num(i,j),1.e-8);
         }
 }
