808bb4ea1a6ab246528c8ef434068f26b6a206b1
Lars Bilke <lars.bilke@ufz.de>
2017-01-30 17:32:57 +0100

    [Jenkins] Spaces and reformatted code.

--- scripts/jenkins/linux.groovy
+++ scripts/jenkins/linux.groovy
@@ -1,56 +1,57 @@
-node('envinf1')
-{
-	dir('ogs') {
-		checkout scm
-	}
+node('envinf1') {
+    dir('ogs') {
+        checkout scm
+    }
 
-	checkout 'https://github.com/ufz/ogs5-benchmarks.git', 'master', 'benchmarks'
-	checkout 'https://github.com/ufz/ogs5-benchmarks_ref.git', 'master', 'benchmarks_ref'
+    checkout 'https://github.com/ufz/ogs5-benchmarks.git', 'master', 'benchmarks'
+    checkout 'https://github.com/ufz/ogs5-benchmarks_ref.git', 'master', 'benchmarks_ref'
 
-	this.runLinux()
+    this.runLinux()
 }
 
 def runLinux() {
-	def configs = [
-		[name:"FEM", cmakeOptions:"-DOGS_USE_CVODE=ON -DOGS_NO_EXTERNAL_LIBS=ON -DCMAKE_BUILD_TYPE=Release", target:"package", artifacts:"*.tar.gz"],
-		[name:"SP"],
-		[name:"GEMS"],
-		[name:"PQC"],
-		[name:"IPQC"],
-		[name:"BRNS"],
-		[name:"MKL", cmakeOptions:"-DMKL_DIR=/opt/intel/mkl"],
-		[name:"LIS"],
-		[name:"MPI"],
-		[name:"PETSC"],
-		[name:"PETSC_GEMS"]
-	]
+    def configs = [
+        [name: "FEM", cmakeOptions: "-DOGS_USE_CVODE=ON -DOGS_NO_EXTERNAL_LIBS=ON " +
+            "-DCMAKE_BUILD_TYPE=Release", target: "package", artifacts: "*.tar.gz"],
+        [name: "SP"],
+        [name: "GEMS"],
+        [name: "PQC"],
+        [name: "IPQC"],
+        [name: "BRNS"],
+        [name: "MKL", cmakeOptions: "-DMKL_DIR=/opt/intel/mkl"],
+        [name: "LIS"],
+        [name: "MPI"],
+        [name: "PETSC"],
+        [name: "PETSC_GEMS"]
+    ]
 
-	stage 'Building'
-	def buildTasks = [:]
-	for(i = 0; i < configs.size(); i++) {
-		def configName = configs[i].name
-		def cmakeOptions = configs[i].cmakeOptions ? configs[i].cmakeOptions : ''
-		cmakeOptions += ' -DNUMDIFF_TOOL_PATH=/usr/local/numdiff/5.8.1-1/bin/numdiff'
-		def target = configs[i].target ? configs[i].target : ''
-		def artifacts = configs[i].artifacts
-		def buildDir = 'build_' + configName
-		buildTasks[configName] = {
-			build buildDir, '-DOGS_CONFIG=' + configName + ' ' + cmakeOptions, target
-			if(artifacts && env.BRANCH_NAME == 'master')
-				archive buildDir + '/' + artifacts
-		}
-	}
-	parallel buildTasks
+    stage 'Building'
+    def buildTasks = [:]
+    for (i = 0; i < configs.size(); i++) {
+        def configName = configs[i].name
+        def cmakeOptions = configs[i].cmakeOptions ? configs[i].cmakeOptions : ''
+        cmakeOptions += ' -DNUMDIFF_TOOL_PATH=/usr/local/numdiff/5.8.1-1/bin/numdiff'
+        def target = configs[i].target ? configs[i].target : ''
+        def artifacts = configs[i].artifacts
+        def buildDir = 'build_' + configName
+        buildTasks[configName] = {
+            build buildDir, '-DOGS_CONFIG=' + configName + ' ' + cmakeOptions, target
+            if (artifacts && env.BRANCH_NAME == 'master')
+                archive buildDir + '/' + artifacts
+        }
+    }
+    parallel buildTasks
 
-	stage 'Benchmarking'
-	def benchmarkTasks = [:]
-	for(i = 0; i < configs.size(); i++) {
-		def configName = configs[i].name
-		def buildDir = 'build_' + configName
-		benchmarkTasks[configName] = {
-			dir(buildDir) {
-				catchError {
-					sh """module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2.10/bin/modulecmd sh \$*`; }
+    stage 'Benchmarking'
+    def benchmarkTasks = [:]
+    for (i = 0; i < configs.size(); i++) {
+        def configName = configs[i].name
+        def buildDir = 'build_' + configName
+        benchmarkTasks[configName] = {
+            dir(buildDir) {
+                catchError {
+                    sh """module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2
+.10/bin/modulecmd sh \$*`; }
 
 set +x
 export MODULEPATH=\"/global/apps/modulefiles:\$MODULEPATH\"
@@ -65,21 +66,21 @@ module load petsc/3.5_maint_gcc_4.8.1-3_openmpi_gcc_1.8.2-1_gcc_4.8.1_CentOS6_en
 export PATH=\"\$PATH:/data/ogs/phreeqc/bin\"
 set -x
 nice -n 5 make benchmarks-short-normal-long"""
-				}
-				archive '**/*.numdiff'
-			}
-		}
-	}
-	parallel benchmarkTasks
+                }
+                archive '**/*.numdiff'
+            }
+        }
+    }
+    parallel benchmarkTasks
 
-	step([$class: 'LogParserPublisher', failBuildOnError: true, unstableOnWarning: true,
-			projectRulePath: 'ogs/scripts/jenkins/log-parser.rules', useProjectRule: true])
+    step([$class: 'LogParserPublisher', failBuildOnError: true, unstableOnWarning: true,
+        projectRulePath: 'ogs/scripts/jenkins/log-parser.rules', useProjectRule: true])
 }
 
 // Builds target with cmakeOptions in buildDir
 def build(buildDir, cmakeOptions, target) {
-	catchError {
-		sh """module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2.10/bin/modulecmd sh \$*`; }
+    catchError {
+        sh """module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2.10/bin/modulecmd sh \$*`; }
 
 set +x
 export MODULEPATH=\"/global/apps/modulefiles:\$MODULEPATH\"
@@ -97,20 +98,21 @@ ln -s /opt/ogs/ogs5-libs Libs || :
 rm -rf ${buildDir} && mkdir ${buildDir} && cd ${buildDir}
 cmake ../ogs ${cmakeOptions} -DCMAKE_BUILD_TYPE=Release
 nice -n 5 make -j 2 ${target}"""
-	}
+    }
 }
 
 // Check out branch from url into directory
 def checkout(url, branch, directory) {
-	stage 'Checkout'
-	checkout([$class: 'GitSCM',
-		branches: [[name: "*/${branch}"]],
-		doGenerateSubmoduleConfigurations: false,
-		extensions:
-			[[$class: 'RelativeTargetDirectory', relativeTargetDir: "${directory}"]],
-		submoduleCfg: [],
-		userRemoteConfigs:
-			[[credentialsId: '6c1dad0d-9b3c-44c2-a6bb-669562045187', url: "${url}"]]])
+    stage 'Checkout'
+    checkout([$class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions:
+            [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${directory}"]],
+        submoduleCfg: [],
+        userRemoteConfigs:
+            [[credentialsId: '6c1dad0d-9b3c-44c2-a6bb-669562045187', url: "${url}"]]])
 }
 
-properties ([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '25']]])
+properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator',
+    artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '25']]])
--- scripts/jenkins/mingw.groovy
+++ scripts/jenkins/mingw.groovy
@@ -1,29 +1,35 @@
 defaultCMakeOptions = '-DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE'
 
-node('docker')
-{
-    step([$class: 'GitHubSetCommitStatusBuilder', statusMessage: [content: 'Started Jenkins mingw build']])
+node('docker') {
+    step([$class: 'GitHubSetCommitStatusBuilder', statusMessage: [content: 'Started Jenkins ' +
+        'mingw build']])
 
     stage 'Checkout'
     dir('ogs') { checkout scm }
 
     docker.image('ogs6/mingw-base').inside()
-    {
-        stage 'Configure'
-        configure 'build', ''
+        {
+            stage 'Configure'
+            configure 'build', ''
 
-        stage 'Build'
-        build 'build', ''
-        if (env.BRANCH_NAME == 'master')
-            build 'build', 'package'
-    }
+            stage 'Build'
+            build 'build', ''
+            if (env.BRANCH_NAME == 'master')
+                build 'build', 'package'
+        }
 
     stage 'Post'
     archive 'build*/*.zip'
-    step([$class: 'S3BucketPublisher', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'opengeosys/ogs5-binaries/head', excludedFile: '', flatten: true, gzipFiles: false, managedArtifacts: true, noUploadOnFailure: true, selectedRegion: 'eu-central-1', sourceFile: 'build*/*.zip', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], profileName: 'S3 UFZ', userMetadata: []])
+    step([$class: 'S3BucketPublisher', dontWaitForConcurrentBuildCompletion: false, entries:
+        [[bucket: 'opengeosys/ogs5-binaries/head', excludedFile: '', flatten: true,
+            gzipFiles: false, managedArtifacts: true, noUploadOnFailure: true,
+            selectedRegion: 'eu-central-1', sourceFile: 'build*/*.zip', storageClass:
+            'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]],
+        profileName: 'S3 UFZ', userMetadata: []])
 
 
-    step([$class: 'GitHubCommitNotifier', resultOnFailure: 'FAILURE', statusMessage: [content: 'Finished Jenkins mingw build']])
+    step([$class: 'GitHubCommitNotifier', resultOnFailure: 'FAILURE', statusMessage:
+        [content: 'Finished Jenkins mingw build']])
 }
 
 def configure(buildDir, cmakeOptions) {
@@ -35,4 +41,5 @@ def build(buildDir, target) {
     sh "cd ${buildDir} && make -j \$(nproc) ${target}"
 }
 
-properties ([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '25']]])
+properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator',
+    artifactDaysToKeepStr: '', artifactNumToKeepStr: '5', daysToKeepStr: '', numToKeepStr: '25']]])
