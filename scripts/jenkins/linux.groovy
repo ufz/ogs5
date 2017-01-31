cc2267fa63c7479f0fb6b6b69a2f526cc9ff2b89
Lars Bilke <lars.bilke@ufz.de>
2017-01-31 09:38:51 +0100

    [Jenkins] Simplified checkout, cleanup build dirs.

--- scripts/jenkins/linux.groovy
+++ scripts/jenkins/linux.groovy
@@ -1,11 +1,12 @@
 def configure = new ogs.configure()
 def build = new ogs.build()
+def post = new ogs.post()
 
 node('envinf1') {
     stage('Checkout') {
         dir('ogs') { checkoutWithTags('https://github.com/ufz/ogs5.git') }
-        checkout 'https://github.com/ufz/ogs5-benchmarks.git', 'master', 'benchmarks'
-        checkout 'https://github.com/ufz/ogs5-benchmarks_ref.git', 'master', 'benchmarks_ref'
+        dir('benchmarks') { git 'https://github.com/ufz/ogs5-benchmarks.git' }
+        dir('benchmarks_ref') { git 'https://github.com/ufz/ogs5-benchmarks_ref.git' }
     }
 
     def configs = [
@@ -23,7 +24,7 @@ node('envinf1') {
         [name: "PETSC_GEMS"]
     ]
 
-    stage('Building') {
+    stage('Build Configs') {
         def buildTasks = [:]
         for (i = 0; i < configs.size(); i++) {
             def config = configs[i]
@@ -46,7 +47,7 @@ node('envinf1') {
                     script: this,
                     target: config.target ? config.target : '')
 
-                if (artifacts && env.BRANCH_NAME == 'master')
+                if (config.artifacts && env.BRANCH_NAME == 'master')
                     archive 'build_' + config.name + '/' + config.artifacts
             }
         }
@@ -56,49 +57,26 @@ node('envinf1') {
     stage('Benchmarking') {
         def benchmarkTasks = [:]
         for (i = 0; i < configs.size(); i++) {
-            def configName = configs[i].name
-            def buildDir = 'build_' + configName
-            benchmarkTasks[configName] = {
-                dir(buildDir) {
-                    catchError {
-                        sh """
-module () { eval `/usr/local/modules/3.2.10-1/Modules/3.2.10/bin/modulecmd sh \$*`; }
-
-set +x
-export MODULEPATH=\"/global/apps/modulefiles:\$MODULEPATH\"
-module load cmake/3.1.3-1
-module load gcc/4.8.1-3
-module load boost/1.55.0-4
-module load doxygen/1.8.7-1_gcc_4.8.1
-module load lapack/3.5.0-1_gcc_4.8.1
-module unload python
-module load openmpi/gcc/1.8.4-2
-module load petsc/3.5_maint_gcc_4.8.1-3_openmpi_gcc_1.8.2-1_gcc_4.8.1_CentOS6_envinf
-export PATH=\"\$PATH:/data/ogs/phreeqc/bin\"
-set -x
-make benchmarks-short-normal-long"""
-                    }
-                    archive '**/*.numdiff'
+            def config = configs[i]
+            benchmarkTasks[config.name] = {
+                catchError {
+                    build.linux(
+                        dir: 'build_' + config.name,
+                        env: 'envinf1/cli.sh',
+                        script: this,
+                        target: 'benchmarks-short-normal-long')
                 }
             }
         }
         parallel benchmarkTasks
+        archive '**/*.numdiff'
     }
 
     stage('Post') {
         step([$class: 'LogParserPublisher', failBuildOnError: true, unstableOnWarning: true,
             projectRulePath: 'ogs/scripts/jenkins/log-parser.rules', useProjectRule: true])
+        for (i = 0; i < configs.size(); i++) {
+            post.cleanup('build_' + configs[i].name)
+        }
     }
-}
-
-// Check out branch from url into directory
-def checkout(url, branch, directory) {
-    checkout([$class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions:
-            [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${directory}"]],
-        submoduleCfg: [],
-        userRemoteConfigs:
-            [[credentialsId: '6c1dad0d-9b3c-44c2-a6bb-669562045187', url: "${url}"]]])
 }
