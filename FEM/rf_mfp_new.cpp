a2437cf5ac17c536d2dfa1edc0daf8390bdd044e
Wenqing Wang <wenqing.wang@ufz.de>
2018-09-01 11:39:02 +0200

    CFluidProperties::drhodT(double* variables): Added the missed case 6

--- FEM/rf_mfp_new.cpp
+++ FEM/rf_mfp_new.cpp
@@ -3365,7 +3365,8 @@ double CFluidProperties::drhodT(double* variables)
 			rho2 = Density(arguments);
 
 			drhodT = (rho1 - rho2) / compressibility_temperature;
-			break;
+		case 6: // rho(p,T) = rho_0*(1+beta_p*(p-p_0)+beta_T*(T-T_0))
+			return drho_dT;
 		case 7: // use of fct file
 			drhodT = 1.0 / T;
 			break;
