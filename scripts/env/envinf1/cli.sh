e20b88a0718d5ac5415e9a38e0d3049bbe1beb0a
Wenqing Wang <wenqing.wang@ufz.de>
2019-06-12 13:28:01 +0200

    [script] Changed modules

--- scripts/env/envinf1/cli.sh
+++ scripts/env/envinf1/cli.sh
@@ -1,18 +1,17 @@
 module use /global/apps/modulefiles
 
+module load foss/2018b
+module load petsc-bilke/3.11.0-foss2018b
+
 module load cmake
-module load gcc/6.2.0-1
-module load openmpi/gcc/1.8.8-1
-module load ninja/1.8.2
+module load ninja/1.9.0
 
 # Libraries
 module load boost/1.55.0-4
 module load doxygen/1.8.7-1_gcc_4.8.1
 module load lapack/gcc/3.7.0-1
-module load petsc/3.7.6_maint_gcc6.2.0_openmpi_gcc_1.8.8-1
 
 # Tools
-module load openmpi/gcc/1.8.8-1
 module load numdiff/5.9_gcc_6.2.0_1
 #module rm gmp/5.1.2-1
 module load gmp-shared/6.1.2-1
